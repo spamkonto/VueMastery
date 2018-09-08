8dd51e5ef9baadf87401180888acc4831b944358
spam.konto <spam.konto@onet.pl>
2018-09-09 01:04:14 +0200

    Started and finished part 2, attribute binding

---
+++ IntroToVue/02.AttributeBinding/jss/app.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		product: 'Socks',
+		description: 'Warm and comfortable socks',
+		image: './ast/vmSocks-green.jpg',
+		altText: 'Pair of Socks',
+		link: 'http://vuemastery.com',
+		linkTarget: '_blank',
+	}
+});
