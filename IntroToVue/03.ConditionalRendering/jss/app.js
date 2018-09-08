6b445705da8fc5f5e18bb218418535090f973c23
spam.konto <spam.konto@onet.pl>
2018-09-09 01:32:29 +0200

    Done with part 4, conditional rendering v-if else-if else and v-show

---
+++ IntroToVue/03.ConditionalRendering/jss/app.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		product: 'Sock',
+		description: 'Warm and comfortable socks',
+		image: './ast/vmSocks-green.jpg',
+		altText: 'Pair of Socks',
+		link: 'http://vuemastery.com',
+		linkTarget: '_blank',
+		inStock: true,
+		inventory: 50,
+		onSale: true,
+	}
+});
